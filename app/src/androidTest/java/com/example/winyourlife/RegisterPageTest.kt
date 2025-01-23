89af890902676a40441a15c6d4914ede97e255e7
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2025-01-23 13:43:59 +0100

    added RegisterPageTest

--- app/src/androidTest/java/com/example/winyourlife/RegisterPageTest.kt
+++ app/src/androidTest/java/com/example/winyourlife/RegisterPageTest.kt
@@ -2,20 +2,18 @@ package com.example.winyourlife
 
 import androidx.activity.compose.setContent
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.winyourlife.domain.AuthenticationService
 import com.example.winyourlife.presentation.MainActivity
 import com.example.winyourlife.presentation.dataObjects.CurrentUser
-import com.example.winyourlife.presentation.loginpage.LoginViewModel
-import com.example.winyourlife.presentation.loginpage.PortraitLayout
 import com.example.winyourlife.presentation.navigation.AppNavHost
 import com.example.winyourlife.presentation.navigation.NavigationScreens
+import com.example.winyourlife.presentation.registerpage.PortraitLayout
+import com.example.winyourlife.presentation.registerpage.RegisterViewModel
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,48 +21,52 @@ import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
-class LoginPageTest {
-
+class RegisterPageTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
-    fun testPortraitLayoutInteractions() {
-        val currentUser:CurrentUser = Mockito.mock(CurrentUser::class.java)
-        val authenticationService : AuthenticationService = Mockito.mock(AuthenticationService::class.java)
-        val fakeViewModel = LoginViewModel(authenticationService,currentUser)
+    fun testRegistrationLayoutInteractions() {
+        val currentUser: CurrentUser = Mockito.mock(CurrentUser::class.java)
+        val authenticationService: AuthenticationService = Mockito.mock(AuthenticationService::class.java)
+        val fakeViewModel = RegisterViewModel(authenticationService, currentUser)
         val navController = TestNavHostController(composeTestRule.activity)
 
-
         composeTestRule.activity.setContent {
-
             navController.navigatorProvider.addNavigator(ComposeNavigator())
             AppNavHost(navController = navController)
             PortraitLayout(viewModel = fakeViewModel, navController = navController)
         }
 
-
-        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.getString(R.string.app_logo_description)).assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.register_hd)).assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.nickname_label)).assertExists()
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.email_label)).assertExists()
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.password_label)).assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.repeat_password_label)).assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.register_button)).assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.register_text)).assertExists()
 
 
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.nickname_label))
+            .performTextInput("testuser")
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.email_label))
             .performTextInput("test@example.com")
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.password_label))
             .performTextInput("password123")
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.repeat_password_label))
+            .performTextInput("password123")
 
-        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.log_in_button))
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.register_button))
             .performClick()
 
+        assert(fakeViewModel.nickname.value == "testuser")
         assert(fakeViewModel.email.value == "test@example.com")
         assert(fakeViewModel.password.value == "password123")
+        assert(fakeViewModel.repeatPassword.value == "password123")
 
-
-        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.sign_up_button))
+        composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.register_text))
             .performClick()
-        assert(navController.currentDestination?.route == NavigationScreens.REGISTER.name)
-
+        assert(navController.currentDestination?.route == NavigationScreens.LOGIN.name)
 
         composeTestRule.onNodeWithText(composeTestRule.activity.getString(R.string.forgot_password_button))
             .performClick()
